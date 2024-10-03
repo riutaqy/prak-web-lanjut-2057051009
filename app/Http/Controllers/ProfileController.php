aff2a8df5d3f606cb837321456d19735bd38a4e1
riutaqy <riutaqy23@gmail.com>
2024-10-03 15:42:19 +0700

    perubahan modul 3

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
-    public function profile($nama = "", $kelas = "", $npm = "")
+    public function profile($nama = '', $kelas = '', $npm = '')
     {
         $data = [
             'nama' => $nama,
